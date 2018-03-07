8bcd72d3095d5f5505761cd4072150d743e01994
Tormod Myrvang <tormod.myvang@gmail.com>
2018-03-07 06:16:33 +0100

    Complete question 3

--- P1/mach0/mach.c
+++ P1/mach0/mach.c
@@ -28,12 +28,24 @@ void unit_test_n3() {
 	printf("%s\n", ans);
 }
 
+void verification_test() {
+	for (int k = 1; k <= 24; k++) {
+		double n = pow(2,k);
+		double pi = approx_pi(n);
+		double error = fabs(M_PI - pi);
+		printf("Error n = 2^%i: %.*g\n", k, DBL_DECIMAL_DIG, error);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("No argument 'n' provided!\n");
 		return 1;
 	} else if (strcmp(argv[1], "utest") == 0) {
 		unit_test_n3();
+		return 0;
+	} else if (strcmp(argv[1], "vtest") == 0) {
+		verification_test();
 		return 0;
 	}
 
--- P1/zeta0/zeta.c
+++ P1/zeta0/zeta.c
@@ -4,9 +4,9 @@
 #include <float.h>
 #include <string.h>
 
-double approx_pi(int n) {	
+double approx_pi(double n) {	
 	double sn = 0;
-	for (int i = 1; i <= n; i++)
+	for (double i = 1; i <= n; i++)
 		sn += 1.0/(i*i);
 	return sqrt(sn*6);
 }
@@ -22,12 +22,24 @@ void unit_test_n3() {
 	printf("%s\n", ans);
 }
 
+void verification_test() {
+	for (int k = 1; k <= 24; k++) {
+		double n = pow(2,k);
+		double pi = approx_pi(n);
+		double error = fabs(M_PI - pi);
+		printf("Error n = 2^%i: %.*g\n", k, DBL_DECIMAL_DIG, error);
+	}
+}
+
 int main(int argc, char *argv[]) {
 	if (argc < 2) {
 		printf("No argument 'n' provided!\n");
 		return 1;
 	} else if (strcmp(argv[1], "utest") == 0) {
 		unit_test_n3();
+		return 0;
+	} else if (strcmp(argv[1], "vtest") == 0) {
+		verification_test();
 		return 0;
 	}
 
